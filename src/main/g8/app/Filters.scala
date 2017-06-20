ffa74b79e86a6a92ac4ca7d75d2edf0b54bf5d74
Nick Karaolis <nick.karaolis@digital.hmrc.gov.uk>
2017-06-20 13:54:51 +0100

    Added auth filter configuration

--- src/main/g8/app/Filters.scala
+++ src/main/g8/app/Filters.scala
@@ -4,18 +4,25 @@ import javax.inject.Inject
 
 import akka.stream.Materializer
 import com.kenshoo.play.metrics.MetricsFilter
+import com.typesafe.config.Config
+import net.ceedubs.ficus.Ficus._
 import play.api.http.DefaultHttpFilters
 import play.api.mvc._
-import play.api.{Configuration, Logger}
+import play.api.{Configuration, Environment, Logger}
 import uk.gov.hmrc.play.audit.filters.AuditFilter
 import uk.gov.hmrc.play.audit.http.config.LoadAuditingConfig
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
+import uk.gov.hmrc.play.auth.controllers.AuthParamsControllerConfig
+import uk.gov.hmrc.play.auth.microservice.connectors.AuthConnector
+import uk.gov.hmrc.play.auth.microservice.filters.AuthorisationFilter
 import uk.gov.hmrc.play.config.RunMode
+import uk.gov.hmrc.play.config.inject.ServicesConfig
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class Filters @Inject()(loggingFilter: LoggingFilter, auditFilter: MicroserviceAuditFilter, metricsFilter: MetricsFilter)
-  extends DefaultHttpFilters(loggingFilter, auditFilter, metricsFilter)
+class Filters @Inject()(loggingFilter: LoggingFilter, auditFilter: MicroserviceAuditFilter, metricsFilter: MetricsFilter,
+authFilter: MicroserviceAuthFilter)
+  extends DefaultHttpFilters(loggingFilter, auditFilter, metricsFilter, authFilter)
 
 class LoggingFilter @Inject()(implicit val mat: Materializer, ec: ExecutionContext) extends Filter {
 
@@ -47,5 +54,24 @@ class MicroserviceAuditFilter @Inject()(implicit val mat: Materializer, ec: Exec
 
 class MicroserviceAuditConnector @Inject() extends AuditConnector with RunMode {
   override lazy val auditingConfig = LoadAuditingConfig(s"auditing")
+}
+
+class MicroserviceAuthFilter @Inject()(implicit val mat: Materializer, ec: ExecutionContext,
+					val authConnector: MicroserviceAuthConnector) extends AuthorisationFilter {
+
+}
+
+class MicroserviceAuthFilter @Inject()(implicit val mat: Materializer, configuration: Configuration,
+                              val authConnector: AuthConn,
+                              val authParamsConfig: AuthConfig) extends AuthorisationFilter {
+  override def controllerNeedsAuth(controllerName: String): Boolean = configuration.getBoolean(s"controllers.$controllerName.needsAuth").getOrElse(false)
+}
+
+class AuthConn @Inject()(val environment: Environment) extends AuthConnector with ServicesConfig {
+  override def authBaseUrl: String = baseUrl("auth")
+}
+
+class AuthConfig @Inject()(configuration: Configuration) extends AuthParamsControllerConfig {
+  override def controllerConfigs: Config = configuration.underlying.as[Config]("controllers")
 }
 
