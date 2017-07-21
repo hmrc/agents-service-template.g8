7e0493808a1f8746b5ef380474dac1f7a617eba2
Nick Karaolis <nick.karaolis@digital.hmrc.gov.uk>
2017-07-21 15:07:49 +0100

    Used correct services config for injection Added template route

--- src/main/g8/app/connectors/BackendConnector.scala
+++ src/main/g8/app/connectors/BackendConnector.scala
@@ -2,14 +2,14 @@ package connectors
 
 import javax.inject.{Inject, Singleton}
 
-import uk.gov.hmrc.play.config.inject.ServicesConfig
+import uk.gov.hmrc.play.config.inject.DefaultServicesConfig
 import uk.gov.hmrc.play.http.{HeaderCarrier, HttpResponse}
 import wiring.WSVerbs
 
 import scala.concurrent.Future
 
 @Singleton
-class BackendConnector @Inject()(servicesConfig: ServicesConfig, http: WSVerbs) {
+class BackendConnector @Inject()(servicesConfig: DefaultServicesConfig, http: WSVerbs) {
 
   lazy val baseURL: String = servicesConfig.baseUrl("soft-drinks-industry-levy")
   lazy val serviceURL = "hello-world"
