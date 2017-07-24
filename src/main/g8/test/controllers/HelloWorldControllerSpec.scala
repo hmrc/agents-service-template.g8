b4a1646c4f3ac6690ebf217e3867c38e1fa0dc7f
Nick Karaolis <nick.karaolis@digital.hmrc.gov.uk>
2017-07-24 08:52:24 +0100

    Added mock dependencies for controller

--- src/main/g8/test/controllers/HelloWorldControllerSpec.scala
+++ src/main/g8/test/controllers/HelloWorldControllerSpec.scala
@@ -1,13 +1,25 @@
 package controllers
 
+import connectors.BackendConnector
+import org.scalatest.BeforeAndAfterEach
 import org.scalatest.mockito.MockitoSugar
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.{Application, Configuration}
+import play.api.i18n.{Messages, MessagesApi}
+import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.play.http.HeaderCarrier
 
-class HelloWorldControllerSpec extends PlaySpec with MockitoSugar with GuiceOneAppPerSuite {
+class HelloWorldControllerSpec extends PlaySpec with MockitoSugar with GuiceOneAppPerSuite with BeforeAndAfterEach {
+
+  implicit override lazy val app: Application = new GuiceApplicationBuilder().
+    disable[com.kenshoo.play.metrics.PlayModule].build()
+
+  implicit val mockMessages: MessagesApi = mock[MessagesApi]
+  implicit val mockConfig: Configuration = mock[Configuration]
+  implicit val backendConnector: BackendConnector = mock[BackendConnector]
 
   val mockHelloWorldController = new HelloWorldController()
 
